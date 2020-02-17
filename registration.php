a016337f9995426ca2609bab7c4506be09cf9755
Pr00xxy <hampus.westman@gmail.com>
2020-02-17 08:38:15 +0100

    fix registration.php componentName variable

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'PrOOxxy_Honeypot',
+    'PrOOxxy_SpamFilter',
     __DIR__
 );
