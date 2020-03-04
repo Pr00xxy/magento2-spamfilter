d88e4636f66702db4ec2d46c33f545d8678fd5d0
Pr00xxy <hampus.westman@gmail.com>
2020-03-04 08:46:18 +0100

    Update license to MIT

--- Model/Rules/Newsletter.php
+++ Model/Rules/Newsletter.php
@@ -1,28 +1,12 @@
 <?php
 /**
+ * Copyright © Hampus Westman 2020
+ * See LICENCE provided with this module for licence details
  *
- * NewsletterBlocker.php
- *
- * This file is part of Foobar.
- *
- * {module_name} is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * {module_name} is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with {module_name}.  If not, see <https://www.gnu.org/licenses/>.
- *
- * @category   Pr00xxy
- * @package    {package_name}
  * @author     Hampus Westman <hampus.westman@gmail.com>
  * @copyright  Copyright (c) 2020 Hampus Westman
- * @license    https://www.gnu.org/licenses/gpl-3.0.html  GPLv3.0
+ * @license    MIT License https://opensource.org/licenses/MIT
+ * @link       https://github.com/Pr00xxy
  *
  */
 declare(strict_types=1);
---
+++ Model/Rules/RulesInterface.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright © Hampus Westman 2020
+ * See LICENCE provided with this module for licence details
+ *
+ * @author     Hampus Westman <hampus.westman@gmail.com>
+ * @copyright  Copyright (c) 2020 Hampus Westman
+ * @license    MIT License https://opensource.org/licenses/MIT
+ * @link       https://github.com/Pr00xxy
+ *
+ */
+
+namespace PrOOxxy\SpamFilter\Model\Rules;
+
+use Zend\Validator\ValidatorInterface;
+
+interface RulesInterface
+{
+    /**
+     * @return ValidatorInterface[]
+     */
+    public function addRules(): array;
+}
